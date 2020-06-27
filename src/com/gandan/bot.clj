0f66d5eade36c1a6320211955e145c39a6470a54
chandra <otama.ariq@gmail.com>
2020-06-28 05:54:08 +0700

    First implementation for comic bot
    Implementation serves minimum features:
    1. fetch latest messages
    2. send welcome message when receiving '/start' message
    3. send latest comic strip when receiving '/latest' message
    
    No error handler is implemented. Any error will terminate app.
    Polling is not implemented so this app will terminate after one run.
    
    To able run it, get (telegram bot
    token)[https://core.telegram.org/bots], and export it as
    `TELEGRAM_BOT_TOKEN` before run `clj -A com.gandan.bot`.

--- src/com/gandan/bot.clj
+++ src/com/gandan/bot.clj
@@ -3,21 +3,89 @@
              [clojure.pprint :as pprint]
              [cheshire.core :as cheshire]))
 
-
-(defn -main []
-  (-> (client/get "https://xkcd.com/info.0.json")
-      pprint/pprint))
-
 (defn parse-resp [json]
   {:img (get json "img")
    :title (get json "title")})
 
-(defn fetch-latest 
-  ([] 
-   (fetch-latest (fn [url] (-> (client/get url)
+(defn fetch-latest-comic
+  ([]
+   (fetch-latest-comic (fn [url] (-> (client/get url)
                                (:body)))))
   ([fetcher]
    (-> (fetcher "https://xkcd.com/info.0.json")
        (cheshire/parse-string)
        (parse-resp))))
 
+(def bot-token (System/getenv "TELEGRAM_BOT_TOKEN"))
+(def bot-api "https://api.telegram.org/bot")
+(def bot-methods
+  {:getUpdates "/getUpdates"
+   :sendPhoto "/sendPhoto"
+   :sendMessage "/sendMessage"})
+
+(defn fetch-latest-messages []
+  (-> (client/get (str bot-api bot-token (:getUpdates bot-methods)))
+      (:body)
+      (cheshire/parse-string)))
+
+(defn send-image [chat-id url]
+  (-> (client/post (str bot-api bot-token (:sendPhoto bot-methods))
+                   {:form-params {:chat_id chat-id :photo url}})
+      (:body)
+      (cheshire/parse-string)))
+
+(defn send-message [chat-id txt]
+  (-> (client/post (str bot-api bot-token (:sendMessage bot-methods))
+                   {:form-params {:chat_id chat-id :text txt}})
+      (:body)
+      (cheshire/parse-string)))
+
+(defn bot-send-msg-cmd [chat-id msg]
+  {:cmd :send-text
+   :chat-id chat-id
+   :text msg})
+
+(defn bot-send-img-cmd [chat-id img-url]
+  {:cmd :send-image 
+   :chat-id chat-id 
+   :img-url img-url})
+
+(defn bot-handle-messages [messages]
+  (loop [[msg & rst] messages
+         result []]
+    (if msg
+      (let [chat-id (:chat-id msg)
+            text (:text msg)]
+        (recur rst
+               (condp #(= %1 %2) text
+                 "/start" (conj result (bot-send-msg-cmd chat-id  "Welcome to prototype comic bot!"))
+                 "/latest" (conj result (bot-send-img-cmd chat-id (:img (fetch-latest-comic))))
+                 result)))
+      result)))
+
+(defn parse-telegram-updates [updates]
+  (loop [[upd & rst] updates
+         result []]
+    (if upd
+      (->> {:chat-id (get-in upd ["message" "chat" "id"])
+            :text (get-in upd ["message" "text"])}
+           (conj result)
+           (recur rst))
+      result)))
+
+(defn bot-handle-cmd [commands]
+  (loop [[cmd & rst] commands]
+    (if cmd
+      (let [chat-id (:chat-id cmd)]
+        (condp #(= %1 %2) (:cmd cmd)
+          :send-text (send-message chat-id (:text cmd))
+          :send-image (send-image chat-id (:img-url cmd)))
+        (recur rst)))))
+
+(defn -main []
+  (-> (fetch-latest-messages)
+      (get-in ["result"])
+      (parse-telegram-updates)
+      (bot-handle-messages)
+      (bot-handle-cmd)
+      (pprint/pprint)))
--- test/com/gandan/bot_test.clj
+++ test/com/gandan/bot_test.clj
@@ -2,20 +2,40 @@
   (:require [clojure.test :refer :all]
             [com.gandan.bot :refer :all]))
 
-(deftest integration-test
+(deftest integration-test-get-comic
   (testing "fetch latest comic from xkcd"
-    (let [latest (fetch-latest)]
+    (let [latest (fetch-latest-comic)]
       (is (contains? latest :title))
       (is (contains? latest :img)))))
 
-(deftest unit-test
-  (testing "parse json response resulted map"
+(deftest integration-test-with-telegram-api
+ (testing "fetch latest messages"
+    (let [resp (fetch-latest-messages)]
+      (is (get resp "ok"))))
+
+  (testing "send image url"
+    (let [resp (send-image 40708419 "https://imgs.xkcd.com/comics/modeling_study.png")]
+      (is (get resp "ok"))))
+
+  (testing "send message"
+    (let [resp (send-message 40708419 "Welcome to prototype comic bot")]
+      (is (get resp "ok")))))
+
+(deftest unit-test-get-comic
+  (testing "convert json response into map"
     (let [latest (parse-resp { "img" "some-image-url" "title" "some-title"})]
       (is (contains? latest :img))
       (is (contains? latest :title))))
-  
-  (testing "fetching with fake fetcher"
+
+  (testing "fetch latest comic with fake fetcher"
     (let [fake-resp "{ \"img\": \"some-image-url\", \"title\": \"some-title\" }"
-          fake-fetcher (fn [url] fake-resp) 
-          latest (fetch-latest fake-fetcher)]
-        (is (= "some-image-url" (latest :img))))))
+          fake-fetcher (fn [url] fake-resp)
+          latest (fetch-latest-comic fake-fetcher)]
+      (is (= "some-image-url" (latest :img))))))
+
+(deftest bot-test
+  (testing "bot handle incoming messages"
+    (let [commands (bot-handle-messages [{:chat-id 1 :text "/start"}
+                                         {:chat-id 2 :text "/latest"}])]
+      (is (= [{:cmd :send-text :chat-id 1 :text "Welcome to prototype comic bot!"}
+              {:cmd :send-image :chat-id 2 :img-url "some-url"}] commands)))))
