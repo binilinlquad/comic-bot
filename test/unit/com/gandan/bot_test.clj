a25fe8e055d5f8632ba9d45cb310f80809ec79cc
chandra <otama.ariq@gmail.com>
2020-07-02 06:06:09 +0700

    Split unit-test-get-comit to two tests
    Reorganize scenarios to two different tests because those tests not
    having relation altogether and should reside different test.

--- test/unit/com/gandan/bot_test.clj
+++ test/unit/com/gandan/bot_test.clj
@@ -2,15 +2,15 @@
   (:require [clojure.test :refer :all]
             [com.gandan.bot :refer :all]))
 
-(deftest unit-test-get-comic
+(deftest convert-json-to-map
   (testing "convert json response into map"
     (let [latest (parse-resp {"img" "some-image-url" "title" "some-title"})]
       (is (contains? latest :img))
-      (is (contains? latest :title))))
+      (is (contains? latest :title)))))
 
-  (testing "fetch latest comic with fake fetcher"
+(deftest fetch-latest-xkcd-comic
+  (testing "fetch latest Xkcd comic with fake fetcher"
     (let [fake-resp "{ \"img\": \"some-image-url\", \"title\": \"some-title\" }"
           fake-fetcher (fn [url] fake-resp)
           latest (fetch-latest-comic fake-fetcher)]
       (is (= "some-image-url" (latest :img))))))
-
