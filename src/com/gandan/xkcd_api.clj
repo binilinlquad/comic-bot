92a8b86bb69ddda2177d72a14da438fd140bdc57
chandra <otama.ariq@gmail.com>
2020-07-27 22:05:27 +0700

    Simplify fetch latest comic implementation
    Prepare to more refactoring in future:
    - Duplicate response-to-json from bot.clj
    - Remove capability to replace fetching implementation
    - Remove integration test that only testing parse-resp

--- src/com/gandan/xkcd_api.clj
+++ src/com/gandan/xkcd_api.clj
@@ -2,15 +2,14 @@
   (:require [clj-http.client :as client]
             [cheshire.core :as cheshire]))
 
+(defn response-to-json [response]
+  (cheshire/parse-string (:body response)))
+
 (defn parse-resp [json]
   {:img (get json "img")
    :title (get json "title")})
 
-(defn fetch-latest-comic
-  ([]
-   (fetch-latest-comic (fn [url] (-> (client/get url)
-                                     (:body)))))
-  ([fetcher]
-   (-> (fetcher "https://xkcd.com/info.0.json")
-       (cheshire/parse-string)
-       (parse-resp))))
+(defn fetch-latest-comic []
+   (-> (client/get "https://xkcd.com/info.0.json")
+       (response-to-json)
+       (parse-resp)))
--- test/unit/com/gandan/xkcd_api_test.clj
+++ test/unit/com/gandan/xkcd_api_test.clj
@@ -7,10 +7,3 @@
     (let [latest (parse-resp {"img" "some-image-url" "title" "some-title"})]
       (is (contains? latest :img))
       (is (contains? latest :title)))))
-
-(deftest fetch-latest-xkcd-comic
-  (testing "fetch latest Xkcd comic with fake fetcher"
-    (let [fake-resp "{ \"img\": \"some-image-url\", \"title\": \"some-title\" }"
-          fake-fetcher (fn [url] fake-resp)
-          latest (fetch-latest-comic fake-fetcher)]
-      (is (= "some-image-url" (latest :img))))))
