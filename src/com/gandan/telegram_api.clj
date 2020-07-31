d4c804636d2dc26c13454e896826ef41e8d2fb44
chandra <otama.ariq@gmail.com>
2020-07-31 19:21:52 +0700

    Rename alias client to http
    Give better alias because read http/client or other http operation is
    better than client/get.

--- src/com/gandan/telegram_api.clj
+++ src/com/gandan/telegram_api.clj
@@ -1,5 +1,5 @@
 (ns com.gandan.telegram-api
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as http]))
 
 (defn- create-endpoint [baseUrl token api-path]
   (str baseUrl token "/" api-path))
@@ -12,18 +12,18 @@
 (deftype Client [baseUrl token]
   Api
   (fetch-latest-messages [this]
-    (client/get (create-endpoint baseUrl token "getUpdates")))
+    (http/get (create-endpoint baseUrl token "getUpdates")))
 
   (fetch-latest-messages [this offset]
-    (client/get (create-endpoint baseUrl token "getUpdates")
+    (http/get (create-endpoint baseUrl token "getUpdates")
                 {:query-params {"offset" (inc offset)}}))
 
   (send-image [this chat-id url]
-    (client/post (create-endpoint baseUrl token "sendPhoto")
+    (http/post (create-endpoint baseUrl token "sendPhoto")
                  {:form-params {:chat_id chat-id :photo url}}))
 
   (send-message [this chat-id txt]
-    (client/post (create-endpoint baseUrl token "sendMessage")
+    (http/post (create-endpoint baseUrl token "sendMessage")
                  {:form-params {:chat_id chat-id :text txt}})))
 
 
