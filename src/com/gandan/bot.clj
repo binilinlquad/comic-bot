1ac9b8f997a0eb2c1dfbcc3dd3707b7e7f90ba62
chandra <otama.ariq@gmail.com>
2020-10-25 15:42:49 +0700

    Split polling implementation from processing its result

--- src/com/gandan/bot.clj
+++ src/com/gandan/bot.clj
@@ -59,19 +59,23 @@
        (last)
        (get "update_id")))
 
-(defn bot-polling []
-  (loop [latest-update-id nil]
-    (log/info "fetch and process latest messages")
-    (let [updates (if latest-update-id
-                    (telegram/fetch-latest-messages latest-update-id)
+(defmacro polling-latest-updates [& forms]
+  `(loop [latest-update-id# nil]
+    (log/info "fetch and process latest chats")
+    (let [updates# (if latest-update-id#
+                    (telegram/fetch-latest-messages latest-update-id#)
                     (telegram/fetch-latest-messages))]
-      (-> (get updates "result")
-          (parse-telegram-updates)
-          (bot-convert-messages-to-commands)
-          (bot-handle-cmd))
-      (log/info "sleep 1 minute")
-      (Thread/sleep (* 1 60 1000))
-      (recur (get-latest-update-id updates)))))
+      (-> (get updates# "result")
+          ~@forms)
+      (log/info "next poll in 1 minute")
+      (Thread/sleep (* 1 60 1000))      
+      (recur (get-latest-update-id updates#)))))
+
+(defn bot-polling []
+  (polling-latest-updates
+    parse-telegram-updates
+    bot-convert-messages-to-commands
+    bot-handle-cmd))
 
 (defn -main []
   (do (log/info "Start up Bot")
