6481303e9956f1e72619ba893ba7178575503dbb
chandra <otama.ariq@gmail.com>
2021-08-24 21:09:39 +0700

    Remove invalid test

--- test/unit/com/gandan/bot_test.clj
+++ test/unit/com/gandan/bot_test.clj
@@ -2,8 +2,3 @@
   (:require [clojure.test :refer :all]
             [com.gandan.bot :refer :all]))
 
-(deftest convert-json-to-map
-  (testing "convert json response into map"
-    (let [latest (parse-xkcd-latest-resp {"img" "some-image-url" "title" "some-title"})]
-      (is (contains? latest :img))
-      (is (contains? latest :title)))))
