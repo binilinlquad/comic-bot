e680469012efc781ae69d0e7ed2f7c04b325d577
chandra <otama.ariq@gmail.com>
2023-07-12 22:11:32 +1000

    inline logger inside bot-poll

--- src/main/com/gandan/comic_bot/polling.clj
+++ src/main/com/gandan/comic_bot/polling.clj
@@ -5,14 +5,14 @@
             [com.gandan.comic-bot.handler :as handler]))
 
 (defn bot-poll
-  [ch act poll log]
-  (log "Start up Bot")
+  [ch act poll]
+  (logger/info "Start up Bot")
   (go-loop [offset nil] 
     (let [polling (poll)
           cmd (<! ch)]
       (condp = cmd
         :stop
-        (do (log "Shut down Bot")
+        (do (logger/info "Shut down Bot")
             (close! polling)
             (close! ch))
 
@@ -32,6 +32,5 @@
                          last-id (last handled)]
                      (if last-id (inc last-id) nil)))
                  #(go (<! (timeout 10000)) 
-                      (>!! ch :fetch))
-                 #(logger/info %))
+                      (>!! ch :fetch)))
     ch))
