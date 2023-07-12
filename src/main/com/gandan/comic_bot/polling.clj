8085cc6b04ab2a8ab888f7447dcb6c79a55f74e6
chandra <otama.ariq@gmail.com>
2023-07-12 22:15:36 +1000

    extract main bot logic

--- src/main/com/gandan/comic_bot/polling.clj
+++ src/main/com/gandan/comic_bot/polling.clj
@@ -1,13 +1,13 @@
 (ns com.gandan.comic-bot.polling
-  (:require [clojure.tools.logging :as logger] 
+  (:require [clojure.tools.logging :as logger]
             [clojure.core.async :refer [>!! <! chan go go-loop alts! timeout close!]]
-            [com.gandan.comic-bot.telegram-client :as telegram] 
+            [com.gandan.comic-bot.telegram-client :as telegram]
             [com.gandan.comic-bot.handler :as handler]))
 
 (defn bot-poll
   [ch act poll]
   (logger/info "Start up Bot")
-  (go-loop [offset nil] 
+  (go-loop [offset nil]
     (let [polling (poll)
           cmd (<! ch)]
       (condp = cmd
@@ -21,16 +21,19 @@
   ;; fetch when startup
   (>!! ch :fetch))
 
+(defn- fetch-and-process
+  [offset]
+  (let [resp-body (telegram/fetch-updates offset)
+        updates (get resp-body :result)
+        handled (doall (pmap handler/handle updates))
+        last-id (last handled)]
+    (if last-id (inc last-id) nil)))
 (defn spawn-bot
   []
   (let [ch (chan)]
-    (bot-poll ch
-                 (fn [offset] 
-                   (let [resp-body (telegram/fetch-updates offset) 
-                         updates (get resp-body :result)
-                         handled (doall (pmap handler/handle updates))
-                         last-id (last handled)]
-                     (if last-id (inc last-id) nil)))
-                 #(go (<! (timeout 10000)) 
-                      (>!! ch :fetch)))
+    (bot-poll
+     ch
+     fetch-and-process
+     #(go (<! (timeout 10000))
+          (>!! ch :fetch)))
     ch))
