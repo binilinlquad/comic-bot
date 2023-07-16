4904ddf441d65d6e43845ea80b768e068cc66b41
chandra <otama.ariq@gmail.com>
2023-07-16 18:27:26 +1000

    Destructure netsted map and small reformat

--- src/main/com/gandan/comic_bot/handler.clj
+++ src/main/com/gandan/comic_bot/handler.clj
@@ -8,15 +8,15 @@
   [map-ch]
   (swap! handlers merge map-ch))
 
-(defn get-handler[command]
+(defn get-handler
+  [command]
   (get @handlers command (fn [& _] nil)))
 
 (defn parse-incoming-text [txt]
   (split txt #"\s" 2))
 
-(defn handle [update]
-  (let [msg (:message update)
-        text (:text msg)
-        [cmd & _] (parse-incoming-text text)] 
-    ((get-handler cmd) (get-in msg [:chat :id]))
+(defn handle 
+  [{:keys [message], {:keys [text]} :message}]
+  (let [[cmd & _] (parse-incoming-text text)] 
+    ((get-handler cmd) (get-in message [:chat :id]))
     (:update_id update)))
